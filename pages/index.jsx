0fb4ed3d1f184b2bb87fedc7332cd83a6649012e
Liam Paris <paris.liam@gmail.com>
2022-03-23 13:20:19 -0400

    updated max width

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,12 @@
 import Layout from '../components/layout'
 import { createTagList } from '../lib/tags'
-import { generateHomePosts } from '../lib/posts';
+import { generateHomePosts, getTopPosts } from '../lib/posts';
 import Carousel from '../components/carousel';
 import PostList from '../components/postlist';
-export default function Home({homePosts, tagList }) {
-  const {topPosts, recentPosts, trendingPosts, popularPosts} = homePosts;
+export default function Home({homePosts, topPosts, tagList }) {
+  const {recentPosts, trendingPosts, popularPosts} = homePosts;
   return (
-    <Layout description={"Home"}   tagList={tagList} sidePosts={topPosts} sideTitle="Top Posts">
+    <Layout description={"Home"} tagList={tagList} sidePosts={topPosts} sideTitle="Top Posts">
         <Carousel posts={recentPosts}></Carousel>
         {/*<ThreePostShuffle title={"Trending"} posts={trendingPosts}></ThreePostShuffle>*/}
         <PostList title="Popular" posts={popularPosts}></PostList>
@@ -17,10 +17,12 @@ export default function Home({homePosts, tagList }) {
 export async function getStaticProps() {
   const homePosts = await generateHomePosts(); 
   const tagList = await createTagList();
+  const topPosts = await getTopPosts();
   return {
     props: {
       homePosts,
-      tagList
+      tagList,
+      topPosts
     }
   }
 }
