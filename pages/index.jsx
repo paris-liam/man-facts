888a99564441975e622741d53559b6ea16d321a5
liam <paris.liam@gmail.com>
2022-03-27 13:04:17 -0400

    finding filtered out posts

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,11 +5,12 @@ import Carousel from '../components/carousel';
 import PostList from '../components/postlist';
 export default function Home({homePosts, tagList }) {
   const {topPosts, recentPosts, trendingPosts, popularPosts} = homePosts;
+  const tempPopular = [...trendingPosts, ...popularPosts].slice(0,20); 
   return (
     <Layout description={"Home"}   tagList={tagList} sidePosts={topPosts} sideTitle="Top Posts">
         <Carousel posts={recentPosts}></Carousel>
         {/*<ThreePostShuffle title={"Trending"} posts={trendingPosts}></ThreePostShuffle>*/}
-        <PostList title="Popular" posts={popularPosts}></PostList>
+        <PostList title="Popular" posts={tempPopular}></PostList>
     </Layout>
   )
 }
